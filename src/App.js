061df7be7f4be55ceb3b207de446baa03840966f
matejkrenek <mate23.krenek@gmail.com>
2021-02-27 13:15:21 +0100

    starter setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CakeContainer from './components/CakeContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
